00f1c0541f48a9a809d7a77af624a04784a03ff5
Michael Reay-Beaton <michael@berry.love>
2022-01-11 09:28:04 +0000

    Initial Commit

---
+++ SampleHeaderCollapse/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SampleHeaderCollapse
+//
+//  Created by HSBC on 11/01/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
